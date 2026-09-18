451975fc42895e11c65f842cd85b47c4b34e6110
Eduardo Hideki Sakamoto <eduardo.h.sakamoto@gmail.com>
2026-09-18 01:05:23 -0700

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < operations.length; i++){
+            
+            if (stack.isEmpty()){
+                stack.push(Integer.parseInt(operations[i]));
+            } else if (operations[i].equals("+")){
+                int temp = stack.pop();
+                int sum = stack.peek();
+                stack.push(temp);
+                stack.push(sum+temp);
+            } else if (operations[i].equals("D")){
+                stack.push(stack.peek()*2);
+            } else if (operations[i].equals("C")){
+                stack.pop();
+            } else {
+                stack.push(Integer.parseInt(operations[i]));
+            }
+            
+        }
+        int output = 0;
+        while (!stack.isEmpty()){
+            //int temp = stack.pop();
+            output += stack.pop();
+        }
+        return output;
+    }
+}
