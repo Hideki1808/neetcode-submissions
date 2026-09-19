a051037b5e28fa8b620fb7b8e7a8a9eed9cfb771
Eduardo Hideki Sakamoto <eduardo.h.sakamoto@gmail.com>
2026-09-18 17:31:46 -0700

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.java
@@ -0,0 +1,27 @@
+class Solution {
+    /*public String lowerCase (String s){
+        s.toLowerCase();
+        return s;
+    }*/
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int start = 0;
+        int end = s.length()-1;
+
+        while(start <= end){
+            if (!Character.isLetterOrDigit(s.charAt(end))){
+                end--;
+            } else if (!Character.isLetterOrDigit(s.charAt(start))){
+                start++;
+            } else if(s.charAt(start) != s.charAt(end)){
+                return false;
+            }
+            else {
+                start++;
+                end--;
+            }
+        }
+
+        return true;
+    }
+}
